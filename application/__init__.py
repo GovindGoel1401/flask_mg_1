4bd25e11e9986031479f03fbee915ea85f50386d
Govind Goyal <goyalgovind1401@gmail.com>
2025-04-14 00:52:18 +0530

    near about to done

--- application/__init__.py
+++ application/__init__.py
@@ -3,11 +3,12 @@ from flask_pymongo import PyMongo
 
 app= Flask(__name__)
 app.config['SECRET_KEY']='d1784e03c2801c5d5bc580027c6cbc5a0650af00'
-app.config['MONGO_URI']="mongodb+srv://govind1401:<Govin1401>@cluster0.bfcsavf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+app.config['MONGO_URI'] = "mongodb+srv://govind1401:Govin1401@cluster0.bfcsavf.mongodb.net/todo_flask?retryWrites=true&w=majority"
+
 
 
 
 # setup mongoDB
-mongo_client=PyMongo(app)
-db=mongo_client.db
+mongodb_client=PyMongo(app)
+db=mongodb_client.db
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,30 @@
 from application import app
-from flask import render_template
-@app.route("/")
+from flask import render_template, flash, request,redirect
+from datetime import datetime
+
+from application import db 
+from .forms import TodoForm
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     return  render_template("view_todos.html",title="layout page")
+
+@app.route("/add_todo", methods=["GET", "POST"])
+def add_todo():
+    if request.method=='POST':
+        form = TodoForm(request.form)
+        todo_name= form.name.data
+        todo_description= form.description.data
+        completed= form.completed.data
+        
+        db.todo_flask.insert_one({
+            "name":todo_name,
+            "description":todo_description,
+            "completed": completed,
+            "date_completed":datetime.utcnow()
+        })
+        flash("Todo Successfully added","Success")
+        return redirect("/")
+    else:
+            form= TodoForm()
+    return render_template("add_todo.html",form=form)
