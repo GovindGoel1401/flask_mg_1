e4f48bdf46007e03e465ea21ecd9e5b1872ef36f
Govind Goyal <goyalgovind1401@gmail.com>
2025-04-13 16:17:14 +0530

    thoda to hua

---
+++ application/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+
+app= Flask(__name__)
+app.config['SECRET_KEY']='d1784e03c2801c5d5bc580027c6cbc5a0650af00'
+app.config['MONGO_URI']="mongodb+srv://govind1401:<Govin1401>@cluster0.bfcsavf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+
+# setup mongoDB
+mongo_client=PyMongo(app)
+db=mongo_client.db
+from application import routes
---
+++ application/routes.py
@@ -0,0 +1,5 @@
+from application import app
+from flask import render_template
+@app.route("/")
+def index():
+    return  render_template("view_todos.html",title="layout page")
